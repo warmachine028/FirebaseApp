580ff4ecddeeb44098df641b02d0005f6d50f812
warmachine028 <warmachine028@outlook.com>
2022-09-12 01:35:56 +0530

    Added fixes for routes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,5 @@
 import { Container } from "react-bootstrap"
-import AuthProvider from "../context/AuthContext"
+import AuthProvider, { useAuth } from "../context/AuthContext"
 import Signup from "./Signup"
 import Dashboard from "./Dashboard"
 import Login from "./Login"
@@ -7,15 +7,17 @@ import { BrowserRouter, Route, Routes } from "react-router-dom"
 import UpdateProfile from "./UpdateProfile"
 import ForgotPassword from "./ForgotPassword"
 import AddUsers from "./AddUsers"
+import { Navigate } from "react-router-dom"
+const App = () => {
+	const { currentUser } = useAuth()
 
-function App() {
 	return (
 		<AuthProvider>
 			<Container className="d-flex align-items-center justify-content-center" style={{ minHeight: "100vh" }}>
 				<div className="w-100" style={{ maxWidth: 400 }}>
 					<BrowserRouter>
 						<Routes>
-							<Route exact path="/" element={<Dashboard />} />
+							<Route exact path="/" element={currentUser ? <Dashboard /> : <Navigate to="/signup" />} />
 							<Route path="/signup" element={<Signup />} />
 							<Route path="/login" element={<Login />} />
 							<Route path="/update-profile" element={<UpdateProfile />} />
